67d0192c468aa6c9d021c49713ca4e52a08dc0de
SuperclassLabs <sophia@superclasslabs.com>
2026-01-17 15:21:02 -0500

    Delete build.bat

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-
-python -m pip install pyinstaller
-pyinstaller --onefile --windowed --name SimpleMacro app.py
-
-echo.
-echo Build complete. Find the exe in dist\SimpleMacro.exe
-pause
